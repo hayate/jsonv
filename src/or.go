32d445ac3971cc37fab898cf909c62d3ea15cc32
hayate <andrea@byteset.com>
2014-03-22 22:46:00 +0900

    remove tabs to conform with rest of jsonv src

--- src/or.go
+++ src/or.go
@@ -1,7 +1,7 @@
 package jsonv
 
 import (
-	"fmt"
+        "fmt"
 )
 
 /*
@@ -10,19 +10,19 @@ Logical OR. Ensures that one of the validator validate the data. Value in `V1` a
 Note: `V2` is tried only if `V1` doesn't validate.
 */
 type Or struct {
-	V1 Validator
-	V2 Validator
+        V1      Validator
+        V2      Validator
 }
 
 func (self *Or) Validate(data *interface{}) (outdesc string, err error) {
 
-	desc, err1 := self.V1.Validate(data)
-	if err1 == nil {
-		return fmt.Sprintf("Or(V1)->%s", desc), nil
-	}
-	desc, err2 := self.V2.Validate(data)
-	if err2 == nil {
-		return fmt.Sprintf("Or(V2)->%s", desc), nil
-	}
-	return "Or", fmt.Errorf("V1:%s. V2:%s", err1, err2)
+        desc, err1 := self.V1.Validate(data)
+        if err1 == nil {
+                return fmt.Sprintf("Or(V1)->%s", desc), nil
+        }
+        desc, err2 := self.V2.Validate(data)
+        if err2 == nil {
+                return fmt.Sprintf("Or(V2)->%s", desc), nil
+        }
+        return "Or", fmt.Errorf("V1:%s. V2:%s", err1, err2)
 }
